50e66a37cf917f9cc599d43baf4bf13b6b2c66f1
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-17 11:42:02 +0530

    Update ContentView.swift

--- PullToRefresh/ContentView.swift
+++ PullToRefresh/ContentView.swift
@@ -7,15 +7,76 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct User: Codable {
+    let name: String
+    let email: String
+    
+    init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
+}
+
+struct UserViewModel: Identifiable {
+    var id = UUID().uuidString
+    var user: User
+}
+
+class UserViewModel2: ObservableObject {
+    @Published var users = [
+        UserViewModel(
+            user: User(name: "Sajed", email: "hello@gmail.com")
+        )
+    ]
+    
+    func refreshUsers() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else {
+            return
         }
-        .padding()
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let newUsers = try JSONDecoder().decode([User].self, from: data)
+                DispatchQueue.main.async {
+                    self.users.append(contentsOf: newUsers.compactMap({
+                        return UserViewModel(user: $0)
+                    }))
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+}
+
+struct ContentView: View {
+    
+    @ObservedObject var viewModel = UserViewModel2()
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, content: {
+                List(viewModel.users) { userVM in
+                    Text(userVM.user.name)
+                        .font(.title)
+                        .bold()
+                    Text(userVM.user.email)
+                        .font(.body)
+                }.refreshable {
+                    self.viewModel.refreshUsers()
+                }
+            }).navigationTitle("Friend List")
+        }
     }
 }
 
