753668a04e221678421340f8e7881c3eee7dc192
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-17 10:47:22 +0530

    Initial Commit

---
+++ PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Sajed Shaikh on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
